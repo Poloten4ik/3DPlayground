c43b817c773d8d1ed1176a13c0c5b971ca519390
Murad <krekotnev@gmail.com>
2021-02-09 20:53:57 +0300

    Lesson commit. Added Dotween

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DoorController : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 10f;
+
+    void Start()
+    {
+        
+    }
+
+
+
+    void Update()
+    {
+        //Vector3 newPosition = transform.position;
+        //newPosition.y += speed * Time.deltaTime;
+        //transform.position = newPosition;
+
+        if (transform.position.y < 40)
+        {
+            transform.DOMoveY(40, 2f);
+        }
+        
+    }
+}
