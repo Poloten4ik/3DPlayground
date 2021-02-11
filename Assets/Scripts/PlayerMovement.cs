da39fca5d4453d51fffba4d0e2ddd2dcc9e82c36
Murad <krekotnev@gmail.com>
2021-02-11 20:52:33 +0300

    Lesson commit. Dotween

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public static PlayerMovement Instance { get; private set; }
+    
+
     [Header("Movement config")]
     [SerializeField] private float moveSpeed = 10f;
 
@@ -20,8 +25,58 @@ public class PlayerMovement : MonoBehaviour
 
     private float gravity;
 
+    private Vector3 startPosition;
+    private bool isResetting;
+
+    public void DoDamage()
+    {
+        //TODO check lives;
+
+        ResetPosition();
+    }
+
+    IEnumerator ResetPositionCoroutine()
+    {
+        isResetting = true;
+        transform.position = startPosition;
+        yield return new WaitForSeconds(0.1f);
+        isResetting = false;
+    }
+
+    private void ResetPosition()
+    {
+        isResetting = true;
+        transform.DOMove(startPosition, 1f).OnComplete(FinishReset);
+        //StartCoroutine(ResetPositionCoroutine());
+    }
+
+    private void FinishReset()
+    {
+        isResetting = false;
+    }
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
     private void Update()
     {
+        if(isResetting)
+        {
+            return;
+        }
+
         Rotate();
         Move();
     }
