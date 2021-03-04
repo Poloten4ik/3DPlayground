4105a6776b5e09094c0ce798f0bd82bd83dc1f58
Murad <krekotnev@gmail.com>
2021-03-04 18:58:12 +0300

    Lesson commit

---
+++ Assets/Scripts/Objects/Torch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Damageable))]
+public class Torch : MonoBehaviour
+{
+    
+    [SerializeField] ParticleSystem fireEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Damageable damageable = GetComponent<Damageable>();
+        damageable.OnRecieveDamage += ActivateTorct;
+    }
+
+   private void ActivateTorct(int damage)
+    {
+        print("active");
+      
+
+        if (fireEffect.isPlaying)
+        {
+            fireEffect.Stop();
+        }
+        else
+        {
+            fireEffect.Play();
+        }
+           
+       // GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+}
