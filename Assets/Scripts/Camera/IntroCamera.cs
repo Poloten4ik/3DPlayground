cd20a7dada03e31626a691579c0601bbdeb60811
Murad <krekotnev@gmail.com>
2021-03-11 20:52:03 +0300

    Intro Camera

---
+++ Assets/Scripts/Camera/IntroCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class IntroCamera : MonoBehaviour
+{
+    public UnityEvent OnIntroStart;
+    public UnityEvent OnIntroEnd;
+
+    public void IntroStarted()
+    {
+        OnIntroStart.Invoke();
+    }
+
+    public void IntroFinish()
+    {
+        OnIntroEnd.Invoke();
+    }
+}
